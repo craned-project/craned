08327427c7974d27c144dc1b8ff53348878eab4c
ItsPungpond98 <pongspanichl@gmail.com>
2023-08-08 21:47:10 +0700

    Added Post Component (String Type Problem)

--- stories/Post.tsx
+++ stories/Post.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 
-type ButtonProps = {
-  children: React.ReactNode
+
+type PostProps = {
+  name: React.ReactNode
+  username: React.ReactNode
+  date: React.ReactNode
+  description: React.ReactNode
+  image: React.ReactNode
+  pfp: React.ReactNode
 }
 
-const Button = ({ children, name, date, description, image, pfp ...rest }: ButtonProps) => {
+const Post = ({name, username, date, description, image, pfp,}: PostProps) => {
   return (
-    <button className="button" {...rest}>
-      {children}
-    </button>
+    <div className="p-3 w-1/3 rounded-2xl" style={{backgroundColor: 'var(--darksec)'}}>
+        <div className="flex pb-3 w-full">
+            <img className="rounded-full" style={{width: '50px', height: '50px'}} src={pfp}></img>
+            <div className="pl-3">
+                <div className="text-lg m-0 p-0" style={{color: 'var(--lightpri)'}}>{name}</div>
+                <div className="flex items-center">
+                    <div className="text-sm text-gray-300 m-0 p-0">{username}</div>
+                    <div style={{width: '.3rem', height: '.3rem'}} className="rounded-full bg-slate-500 m-1"></div>
+                    <div className="text-sm m-0 p-0" style={{color: 'var(--lightbg)'}}>{date}</div>
+                </div>
+            </div>
+        </div>
+        <img className="box h-auto object-cover m-auto rounded-md" style={{width: '400px'}} src={image}></img>
+        <p className="text-sm mt-3" style={{color: 'white'}}>{description}</p>
+    </div>
   )
 }
 
-export default Button;
+export default Post;
