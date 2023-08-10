49cee9b0f23c2fc6abe5a8dfd4c59865c3713486
ItsPungpond98 <pongspanichl@gmail.com>
2023-08-10 21:54:00 +0700

    Finished Post Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import Post from '@/stories/Post'
 
 export default function Home() {
   return (
-    <></>
+    <Post name='hello' username='Ee' date='9 Aug 2023' pfp='https://storage.googleapis.com/pai-images/cb1d89d709204f5589e0a6714433c471.jpeg' desc='Hello!' image = 'https://storage.googleapis.com/pai-images/cb1d89d709204f5589e0a6714433c471.jpeg'></Post>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,10 +9,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        lightbg: "#FDE8E9",
+        lightpri: "#E3BAC6",
+        lightsec: "#BC9EC1",
+        darkpri: "#2B59C3",
+        darksec: "#253C78",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        overpass: ['overpass', 'sans'],
       },
     },
   },
