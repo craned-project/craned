30059a79352e018113271d66084fe1e867865ef4
bencinn <iloveheapsort@gmail.com>
2023-09-08 20:28:23 +0700

    forgot check

--- app/(root)/new/page.tsx
+++ app/(root)/new/page.tsx
@@ -2,6 +2,8 @@ import { NewPost } from "@/stories/newpost";
 import { redirect } from "next/navigation";
 import { CheckUser, FetchUser } from "@/lib/actions/updateUser.action";
 import { currentUser } from "@clerk/nextjs";
+import School from "@/lib/models/school.model";
+import User from "@/lib/models/user.model";
 /*
     user: {
         id: string;
@@ -17,6 +19,10 @@ export default async function Page() {
     if (!authuser) return null;
     const userInfo = await FetchUser(authuser.id);
     const check = await CheckUser(authuser.id);
+    // School.create({slug: "pkw", name: "Pkw", admin: await User.findOne({id: authuser.id}), members: [await User.findOne({id: authuser.id})]});
+    if (!check) {
+        redirect("/");
+    }
     return (
         <div className="flex">
             <h1 className="text-3xl">New post</h1>
