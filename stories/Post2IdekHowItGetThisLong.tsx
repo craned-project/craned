ffffb30ca5d08788bae7f292427a54110653546b
hansnnn <iloveheapsort@gmail.com>
2023-09-13 23:40:23 +0700

    add share button

--- stories/Post2IdekHowItGetThisLong.tsx
+++ stories/Post2IdekHowItGetThisLong.tsx
@@ -3,6 +3,7 @@ import Post from "@/lib/models/post.model";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import LikeButton from "./likebtn";
+import { Share } from "./share";
 
 export async function PoooooooseOrPostWhateverDonTMatter({post}: {post: Post}) {
     const user = await currentUser();
@@ -12,6 +13,7 @@ export async function PoooooooseOrPostWhateverDonTMatter({post}: {post: Post}) {
         <div>
             <div className="text-white text-lg">{post.text}</div>
             <div className="flex justify-center items-center">{post.image ? (<Image src={post.image} width={400} height={400} alt="Image" className="bg-pri object-cover rounded-2xl w-[400px] h-[400px]"/>) : <div></div>}</div>
+            <Share url={`${process.env.VERCEL_URL || "localhost:3000"}/posts/${post._id}`} />
             <LikeButton userid={user.id} postid={post._id} likes={likecount} isliking={await getUserLikePostOrNot(user.id, post._id)}/>
         </div>
     )
