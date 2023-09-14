ac4ab9750cb3fa3be1e1e2ccf7cd151b411fa0d3
ItsPungpond98 <pongspanichl@gmail.com>
2023-09-14 12:51:06 +0700

    Styled All school page

--- app/(root)/schools/page.tsx
+++ app/(root)/schools/page.tsx
@@ -1,10 +1,36 @@
 import { getSchool } from "@/lib/actions/schools.action"
 import School from "@/lib/models/school.model";
+import Link from "next/link"
+import Image from "next/image"
 
 export default async function SchoolMain() {
     const schools = await getSchool();
     if (schools === undefined) {
         return <>No schools?</>
     }
-    return <div>{schools.map(school => { return <div>{school.name} ({school.members?.length} student(s)) (admin: {school.admin.name})</div> })}</div>
+    return (
+        <div>
+            <h1 className="text-tri text-3xl font-bold mt-2 mb-2">โรงเรียนทั้งหมด</h1>
+            <div className="flex flex-col gap-2">
+            {schools.map(school => { 
+                    return <div>
+                        <Link href={`/users/${school.name}`} className="flex gap-1 bg-pri p-2 rounded-xl"> 
+                        <Image
+                        src={school.image ? (school.image) : ("../Craned Logo.svg")}
+                        alt="Profile picture"
+                        width={80}
+                        height={80}
+                        className="rounded-full"
+                        />
+                        <div className="flex flex-col">
+                            <div className="text-lg">{school.name}</div>
+                            <div className="text-sm text-gray-600">{school.members?.length} student(s)</div>
+                            <div>Admin: {school.admin.name}</div>
+                        </div> 
+                        </Link>
+                    </div>
+            })}
+            </div>
+        </div>
+        )
 }
