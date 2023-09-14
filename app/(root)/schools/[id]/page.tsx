a1c7c445f225c52e0146a0a1e64e7b89d5f0f2e6
hansnnn <iloveheapsort@gmail.com>
2023-09-14 12:53:30 +0700

    pinned post showing

--- app/(root)/schools/[id]/page.tsx
+++ app/(root)/schools/[id]/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import Post from "@/lib/models/post.model";
 import Test3 from "@/stories/Post3Wow";
 import Image from "next/image"
+import { getPinnedPost } from "@/lib/actions/schools.action";
 export default async function page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   if (!user) return null;
@@ -24,21 +25,20 @@ export default async function page({ params }: { params: { id: string } }) {
     },
   ]);
 
-  console.log("All Posts:", allPosts);
-
+    const pinned: Post[] = await getPinnedPost(params.id);
   const admin: School | null = await User.findOne(school.admin);
 
   console.log(school);
   return (
     <div>
       <div className="flex gap-1 bg-pri p-2 rounded-xl mb-2 mt-3">
-        <Image
+        {school.image && <Image
           src={school.image}
           alt="Profile picture"
           width={80}
           height={80}
           className="rounded-full"
-        />
+        />}
         <div className="flex flex-col">
           <div className="text-xl">{school.name}</div>
           <div>{school.bio ? school.bio : null}</div>
@@ -48,9 +48,11 @@ export default async function page({ params }: { params: { id: string } }) {
             <h2>Admin: None</h2>
           )}</div>
         </div>
-        
+
       </div>
       <div className="flex flex-col gap-2">
+      Pinned Post:
+      {pinned.map((pin) => {pin.text})}
         {allPosts.map((post) => {
           return (
             <div className="flex flex-col gap-2">
